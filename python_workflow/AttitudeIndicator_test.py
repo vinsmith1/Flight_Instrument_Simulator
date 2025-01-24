da3f4a87f022c12caa7aa42b95778c4c9bcae3c4
Vincent Smith <vinsmith1@hotmail.com>
2025-01-23 23:02:16 -0500

    Minor clerical changes to test case code

--- python_workflow/AttitudeIndicator_test.py
+++ python_workflow/AttitudeIndicator_test.py
@@ -1,25 +1,19 @@
 import os
-import VideoWriter
 import AttitudeIndicator
 
-videoFrameRate = 30
-output_path='.\\output\\AI_test'
+OUTPUT_PATH='.\\output\\AI_test'
 
-if not os.path.exists(output_path): 
-        # if the directory is not present then create it. 
-        os.makedirs(output_path) 
+if not os.path.exists(OUTPUT_PATH):
+        # if the directory is not present then create it.
+        os.makedirs(OUTPUT_PATH)
 
 AI = AttitudeIndicator.AttitudeIndicator()
-AI_video = VideoWriter.VideoWriter(AI.size(), videoFrameRate, outputPath=output_path + '\\AI_test.mp4')
 
 # Create image/video sequence
 for i,bank in enumerate(range(-90,91,5)):
     for j,pitch in enumerate(range(-20,21,1)):
         tmpAI = AI.build_image(bank, pitch)
         # Save image
-        tmpAI.save(f'{output_path}\\AI_{i*j:04d}.png')
-        # Write image to video file
-        AI_video.writeFrame(tmpAI)
-        print(f'Pitch: {pitch}, Bank: {bank}')
+        tmpAI.save(f'{OUTPUT_PATH}\\AI_{i*j:04d}.png')
 
-AI_video.release()
+        print(f'Pitch: {pitch}, Bank: {bank}')
