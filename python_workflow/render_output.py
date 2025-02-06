f3e051d3e49d9b4f0821a6d8c63f5a0d6ca2f855
Vincent Smith <vinsmith1@hotmail.com>
2025-02-06 13:35:43 -0500

    Implemented multiprocessing for image rendering leading to significant increase in performance (increased from roughly 11 frames/second to about 160 frames/second on a 28-core i7-13850HX)

--- python_workflow/render_output.py
+++ python_workflow/render_output.py
@@ -1,4 +1,6 @@
+from multiprocessing import Pool
 import InstrumentPanel
+instrument_panel_image = InstrumentPanel.InstrumentPanel()
 
 def render_output(frames, path):
     """Render frame info to output image sequence
@@ -7,10 +9,15 @@ def render_output(frames, path):
         frames (_type_): _description_
         path (_type_): _description_
     """
-    instrument_panel_image = InstrumentPanel.InstrumentPanel()
-    
-
+    p = Pool()
     for frame, altitude, course, speed, bank, pitch in zip(frames['Frame'], frames['Altitude'], frames['Course'], frames['Speed'], frames['Bank'], frames['Pitch']):
-        tmp_image = instrument_panel_image.build_image(altitude, course, speed, bank, pitch)
-        tmp_image.save(f'{path}\\InstrumentPanel_{frame:04d}.png', compress_level=1)
+        p.apply_async(render_frame, args=(altitude, course, speed, bank, pitch, frame, path))
+    p.close()
+    p.join()
 
+def render_frame(altitude, course, speed, bank, pitch, frame, path):
+    tmp_image = instrument_panel_image.build_image(altitude, course, speed, bank, pitch)
+    tmp_image.save(f'{path}\\InstrumentPanel_{frame:04d}.png', compress_level=1)
+
+if __name__ == '__main__':
+    render_output()
